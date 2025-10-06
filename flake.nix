9f9a144b51274d8eda02fdda9fdd569427ef9738
Damian Poddebniak <poddebniak@mailbox.org>
2025-10-06 08:53:05 +0200

    chore: add flake.{nix,lock}

---
+++ flake.nix
@@ -0,0 +1,35 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05-small";
+  };
+
+  outputs =
+    { self, nixpkgs, ... }:
+    let
+      eachSupportedSystem = nixpkgs.lib.genAttrs supportedSystems;
+      supportedSystems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
+
+      mkDevShells =
+        system:
+        let
+          pkgs = import nixpkgs { inherit system; };
+        in
+        {
+          default = pkgs.mkShell {
+            strictDeps = true;
+            nativeBuildInputs = with pkgs; [
+              rustPlatform.bindgenHook
+              rustup
+            ];
+          };
+        };
+    in
+    {
+      devShells = eachSupportedSystem mkDevShells;
+    };
+}
